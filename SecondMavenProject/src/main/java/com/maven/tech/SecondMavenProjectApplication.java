231fe5be5eccece31ffa28d89ef2925bf86947c1
smartprogrammer++ <nikeshsingh9631973678@gmail.com>
2023-08-28 17:55:29 +0530

    Merge branch 'main' of https://github.com/smartprgrammer999/javaassignment.git into main

---
+++ SecondMavenProject/src/main/java/com/maven/tech/SecondMavenProjectApplication.java
@@ -0,0 +1,13 @@
+package com.maven.tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondMavenProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondMavenProjectApplication.class, args);
+	}
+
+}
